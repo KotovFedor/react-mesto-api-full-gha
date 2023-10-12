cfdc602389e8021720d5f25bd451398ceaeb39f4
fedorkotov30 <kotovfedor30@gmail.com>
2023-10-12 23:43:30 +0300

    fix back/signUp.js

--- backend/routes/signUp.js
+++ backend/routes/signUp.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const { celebrate, Joi } = require('celebrate');
 const { createUser } = require('../controllers/users');
-const regexForUrl = require('../utils/constants');
+const { regexForUrl } = require('../utils/constants');
 
 router.post('/signup', celebrate({
   body: Joi.object().keys({
